d2f7cb245511648b2318c8256456b244673fa6a0
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-07-18 07:07:17 +0800

    Camera dragging

--- Assets/Scenes/Main/Scene/Scripts/MainCamera.cs
+++ Assets/Scenes/Main/Scene/Scripts/MainCamera.cs
@@ -1,15 +1,57 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MainCamera : MonoBehaviour {
-	// Start is called before the first frame update
-	void Start() {
+	bool dragging = false;
+	Vector2 lastDrag;
+	static int uiLayer;
+	new Camera camera;
 
+	public void Start() {
+		uiLayer = LayerMask.NameToLayer("UI");
+		camera = GetComponent<Camera>();
 	}
 
-	// Update is called once per frame
-	void Update() {
+	static bool PointerOverUI() {
+		var es = EventSystem.current;
+		if(!es.IsPointerOverGameObject())
+			return false;
+		var ev = new PointerEventData(es);
+		ev.position = Input.mousePosition;
+		var res = new List<RaycastResult>();
+		es.RaycastAll(ev, res);
+		return res.Any(hit => hit.gameObject.layer == uiLayer);
+	}
 
+	void UpdateDraggingState() {
+		if(Input.GetMouseButtonUp(0))
+			dragging = false;
+		if(PointerOverUI())
+			return;
+		if(Input.GetMouseButtonDown(0)) {
+			dragging = true;
+			lastDrag = Input.mousePosition;
+		}
+	}
+
+	void CheckDrag() {
+		if(!dragging)
+			return;
+		Vector2 dragDelta = (Vector2)Input.mousePosition - lastDrag;
+		SendMessage("OnDrag", dragDelta, SendMessageOptions.DontRequireReceiver);
+		lastDrag = Input.mousePosition;
+	}
+
+	public void OnDrag(Vector2 delta) {
+		var from = camera.ScreenToWorldPoint(Vector2.zero);
+		var to = camera.ScreenToWorldPoint(delta);
+		transform.position += from - to;
+	}
+
+	public void Update() {
+		UpdateDraggingState();
+		CheckDrag();
 	}
 }
